621feb9c51632b0a9ef011eb132d54cc02f881cf
Joksan2003 <anjonatividadag@ittepic.edu.mx>
2025-11-25 03:21:04 -0700

    Desplegar con mongo y ruta actualizada

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const adminUsersRoutes = require("./backend/src/routes/adminUsers");
 
 // Conectar a MongoDB
-//connectDB();
+connectDB();
 
 const allowedOrigins = [
   "http://localhost:5173",
